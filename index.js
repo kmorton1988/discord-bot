b49cb909ce468a2ea7a6ee1f57338b1f70073852
Kyle Morton <kyle@gomorton.com>
2020-11-27 19:16:45 -0800

    added config file to gitignore

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 const client = Discord.Client();
-const Fetch = require('fetch.js)');
-const check = Fetch.Data();
+const config = require('config.js')
+const token = config.token;
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-    }
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
 });
 
-client.login('')
+client.login(token)
